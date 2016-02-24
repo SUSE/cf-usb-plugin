d758fe8fe70ceb70d668e79186cb628f7f1685e5
adasescu <ada.sescu@hp.com>
2016-02-24 18:25:01 +0200

    Move GetPlanByID and GetServiceByDriverInstanceID from commands to usb client interface

--- commands/service.go
+++ commands/service.go
@@ -10,7 +10,6 @@ import (
 
 //ServiceInterface exposes service commands
 type ServiceInterface interface {
-	GetServiceByDriverInstanceID(swaggerclient.AuthInfoWriter, string) (*models.Service, error)
 	Update(swaggerclient.AuthInfoWriter, *models.Service) (string, error)
 }
 
@@ -22,16 +21,6 @@ type ServiceCommands struct {
 //NewServiceCommands returns a ServiceCommands object
 func NewServiceCommands(httpClient lib.UsbClientInterface) ServiceInterface {
 	return &ServiceCommands{httpClient: httpClient}
-}
-
-//GetServiceByDriverInstanceID returns a service by driver instance id
-func (c *ServiceCommands) GetServiceByDriverInstanceID(bearer swaggerclient.AuthInfoWriter, driverInstanceID string) (*models.Service, error) {
-	response, err := c.httpClient.GetServiceByInstanceID(&operations.GetServiceByInstanceIDParams{DriverInstanceID: driverInstanceID}, bearer)
-	if err != nil {
-		return nil, err
-	}
-
-	return response.Payload, nil
 }
 
 //Update - updates a service's details
